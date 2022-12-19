64d27f3898121529ae9863f58439c9daa666ee60
cutegyuseok <yigyuseok@gmail.com>
2022-12-19 19:39:52 +0900

    feat: swagger ui setting

--- src/main/java/com/example/bitcoinShopPractice/aop/Test.java
+++ src/main/java/com/example/bitcoinShopPractice/aop/Test.java
@@ -1,28 +1,28 @@
-//package com.example.bitcoinShopPractice.aop;
-//
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.After;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.springframework.http.HttpRequest;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.context.request.RequestContextHolder;
-//import org.springframework.web.context.request.ServletRequestAttributes;
-//
-//import javax.servlet.http.HttpSession;
-//
-//@Aspect
-//@Component
-//public class Test {
-//
-//    @Pointcut("execution(* com.example.bitcoinShopPractice.page.controller.PageController.*(..))")
-//    public void cut(){}
-//
-//    @After("cut()")
-//    public void around(ProceedingJoinPoint joinPoint){
-////        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
-//        String str = joinPoint.getSignature().getName().toString();
-//        System.out.println(str);
-//
-//    }
-//}
+    //package com.example.bitcoinShopPractice.aop;
+    //
+    //import org.aspectj.lang.ProceedingJoinPoint;
+    //import org.aspectj.lang.annotation.After;
+    //import org.aspectj.lang.annotation.Aspect;
+    //import org.aspectj.lang.annotation.Pointcut;
+    //import org.springframework.http.HttpRequest;
+    //import org.springframework.stereotype.Component;
+    //import org.springframework.web.context.request.RequestContextHolder;
+    //import org.springframework.web.context.request.ServletRequestAttributes;
+    //
+    //import javax.servlet.http.HttpSession;
+    //
+    //@Aspect
+    //@Component
+    //public class Test {
+    //
+    //    @Pointcut("execution(* com.example.bitcoinShopPractice.page.controller.PageController.*(..))")
+    //    public void cut(){}
+    //
+    //    @After("cut()")
+    //    public void around(ProceedingJoinPoint joinPoint){
+    ////        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+    //        String str = joinPoint.getSignature().getName().toString();
+    //        System.out.println(str);
+    //
+    //    }
+    //}
